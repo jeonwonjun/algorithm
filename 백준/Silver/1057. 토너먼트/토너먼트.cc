9e4a31a3855acdf4fd475b1fb3e3398e8d52ccfb
WonJ <84172525+jeonwonjun@users.noreply.github.com>
2025-08-10 23:29:41 +0900

    [Silver IV] Title: 토너먼트, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1057. 토너먼트/토너먼트.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+
+int main (){
+    int n, kim, lim;
+    int round = 0;
+    
+    scanf("%d %d %d", &n, &kim, &lim);
+    
+    while(kim != lim){
+        kim = (kim+1)/2;
+        lim = (lim+1)/2;
+        round++;
+    }
+    
+    printf("%d\n", round);
+    
+    return 0;
+}
