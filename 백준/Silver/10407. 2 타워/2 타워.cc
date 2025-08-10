9c78173ace78f804ce141fece3dfa279754f43b2
WonJ <84172525+jeonwonjun@users.noreply.github.com>
2025-08-10 23:23:06 +0900

    [Silver III] Title: 2 타워, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/10407. 2 타워/2 타워.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int n;
+    cin >> n;
+    if (n == 1) {
+        cout << 2 <<'\n';
+    } else {
+        cout << 1 << '\n';
+    }
+    return 0;
+}
