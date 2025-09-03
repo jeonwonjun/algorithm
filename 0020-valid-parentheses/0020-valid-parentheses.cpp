b5c4e108c1ac24ba66df428ad1b128f2c135bc9c
Wonel <84172525+jeonwonjun@users.noreply.github.com>
2025-09-03 23:11:15 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        // input max size is 10000, so time complexity less than O(n^2)
+        stack<char> st;
+        for (int i=0; i<s.size(); i++) {
+            if (s[i] == '(' || s[i] == '[' || s[i] == '{')
+                st.push(s[i]);
+            else if (s[i] == ')' || s[i] == ']' || s[i] == '}') {
+                char k = st.top();
+                if (k == '(' && s[i] == ')')
+                    st.pop();
+                else if (k == '[' && s[i] == ']')
+                    st.pop();
+                else if (k == '{' && s[i] == '}')
+                    st.pop();
+                else
+                    return false;
+            }
+        }
+
+        if (!st.empty())
+            return false;
+
+        return true;
+    }
+};
