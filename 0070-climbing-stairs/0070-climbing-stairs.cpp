93971825fff077f6164c57b52ddd3ff781dc9868
Wonel <84172525+jeonwonjun@users.noreply.github.com>
2025-10-05 19:00:43 +0900

    Time: 0 ms (100%), Space: 7.8 MB (72.76%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -2,11 +2,10 @@ class Solution {
     int d[46];
 public:
     int climbStairs(int n) {
-        // dp: end to 1 or 2
-        
-        d[0] = 1, d[1] = 1;
-        for (int i = 2; i <= 45; i++) {
-            d[i] = d[i-2] + d[i-1];
+        d[0] = 1;
+        d[1] = 1;
+        for(int i=2; i<=n; i++){
+            d[i] = d[i-1] + d[i-2];
         }
 
         return d[n];
