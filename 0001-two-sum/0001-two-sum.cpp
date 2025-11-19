126737698dd2ed3a8399680d7f03efe93496cadf
Wonel <jwjjwj1960@naver.com>
2025-11-19 22:34:02 +0900

    Time: 4 ms (54.76%), Space: 14.8 MB (36.57%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int, int> hash;
-        for (int i=0; i<nums.size(); i++){
-            int complement = target - nums[i];
-            if (hash.find(complement) != hash.end())
-                return{i, hash[complement]};
+        for (int i = 0; i<nums.size(); i++) {
+            int cmp = target - nums[i];
+            if (hash.find(cmp) != hash.end()) {
+                return {i, hash[cmp]};
+            }
 
             hash[nums[i]] = i;
         }
-
         return {};
     }
 };
