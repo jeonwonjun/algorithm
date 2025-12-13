3c54c32d03e2aebd50dd1bc3d18e3f42352f910d
Wonel <jwjjwj1960@naver.com>
2025-12-13 22:20:04 +0900

    [Bronze IV] Title: Football Team, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/5358. Football Team/Football Team.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str;
+    while (getline(cin, str)) {
+        for (int i=0; i<str.length(); i++) {
+            if (str[i] == 'i')
+                str[i] = 'e';
+            else if (str[i] == 'e')
+                str[i] = 'i';
+            else if (str[i] == 'I')
+                str[i] = 'E';
+            else if (str[i] == 'E')
+                str[i] = 'I';
+        }
+        
+        cout << str << '\n';
+    }
+
+    return 0;
+}
