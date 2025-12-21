d958e971c1da33414d7346ac2e2df19b6b6158a7
Wonel <jwjjwj1960@naver.com>
2025-12-21 22:41:00 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 144 ms, Memory: 13732 KB -BaekjoonHub

---
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <map>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool compare(const pair<string, int>& a, const pair<string, int>& b) {
+    if (a.second == b.second) {
+        if (a.first.length() == b.first.length())
+            return a.first < b.first;
+        return a.first.length() > b.first.length();
+    }
+
+    return a.second > b.second;
+}
+
+int main() {
+    map<string, int> dictionaly;
+    int n, m;
+    cin >> n >> m;
+    for (int i=0; i<n; i++) {
+        string word;
+        cin >> word;
+        if (word.length() >= m) {
+            dictionaly[word]++;
+        }
+    }
+
+    vector<pair<string, int>> dic(dictionaly.begin(), dictionaly.end());
+
+    sort(dic.begin(), dic.end(), compare);
+
+    for (auto pair : dic)
+        cout << pair.first << '\n';
+
+    return 0;
+}
