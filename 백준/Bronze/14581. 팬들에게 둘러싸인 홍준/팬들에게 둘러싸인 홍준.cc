e47754a3f337f64e1668c925fc9e4cb3b9664114
Wonel <jwjjwj1960@naver.com>
2025-12-24 12:40:03 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    
+    string id, fan = ":fan:";
+    cin >> id;
+    
+    cout << fan << fan << fan << "\n";
+    cout << fan << ":" << id << ":" << fan << "\n";
+    cout << fan << fan << fan;
+    
+    return 0;
+}
