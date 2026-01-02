5253aa609e1eb9810cc0930b8b0d5b36c16bad8c
Wonel <jwjjwj1960@naver.com>
2026-01-03 00:52:07 +0900

    [Bronze V] Title: Rectangles, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15232. Rectangles/Rectangles.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int r, c;
+    cin >> r;
+    cin >> c;
+    
+    for (int i=0; i<r; i++) {
+        for (int j=0; j<c; j++) {
+            cout <<'*';
+        }
+        cout << '\n';
+    }
+}
