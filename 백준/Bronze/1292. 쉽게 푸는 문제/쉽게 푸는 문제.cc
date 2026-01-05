531c50e04a15e35667b724d6c80d0c21c5c18c72
Wonel <jwjjwj1960@naver.com>
2026-01-06 02:28:47 +0900

    [Bronze I] Title: 쉽게 푸는 문제, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1292. 쉽게 푸는 문제/쉽게 푸는 문제.cc
+++ 백준/Bronze/1292. 쉽게 푸는 문제/쉽게 푸는 문제.cc
@@ -1,39 +1,24 @@
 #include <iostream>
 using namespace std;
 
+int prefixSum(int n) {
+    int num = 1;
+    int cnt = 0;
+    int sum = 0;
+
+    while (cnt < n) {
+        for (int i = 0; i < num && cnt < n; i++) {
+            sum += num;
+            cnt++;
+        }
+        num++;
+    }
+    return sum;
+}
+
 int main() {
     int a, b;
     cin >> a >> b;
-    
-    int i1 = 1;
-    int k1=1;
-    int cnt1 = 1;
-    int sum1 = 0;
-    while (cnt1 <= b) {
-        int j=1;
-        while (j <= k1 && cnt1 <= b) {
-            sum1 += i1;
-            cnt1++;
-            j++;
-        }
-        i1++;
-        k1++;
-    }
-    
-    int i2=1;
-    int k2=1;
-    int cnt2 = 1;
-    int sum2 = 0;
-    while (cnt2 < a) {
-        int j=1;
-        while (j<=k2 && cnt2 < a) {
-            sum2 += i2;
-            j++;
-            cnt2++;
-        }
-        i2++;
-        k2++;
-    }
-    
-    cout << sum1 - sum2;
+
+    cout << prefixSum(b) - prefixSum(a - 1);
 }
