bb297a2fa6122b6371983c3a6116a6859490e05a
Wonel <jwjjwj1960@naver.com>
2026-02-06 02:14:00 +0900

    [Bronze IV] Title: Blaster's Dash, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34380. Blaster's Dash/Blaster's Dash.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    long long M, V, n, K;
+    cin >> M >> V;   // not needed for the distance
+    cin >> n;
+    cin >> K;        // not needed for the distance
+
+    cout << 2 * n + 40 << "\n";
+    return 0;
+}
