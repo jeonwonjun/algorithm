660602ebf1e439ee96d655b09368cd53b129cc7e
Wonel <jwjjwj1960@naver.com>
2026-02-08 23:32:10 +0900

    [Bronze IV] Title: Tri-du, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/13597. Tri－du/Tri－du.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    
+    cin >> a >> b;
+    
+    int answer = max(a, b);
+    
+    cout << answer;
+    
+    return 0;
+}
