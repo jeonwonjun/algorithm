d9ea5ea46b66e43cfe5662b561cc83881a532827
Wonel <jwjjwj1960@naver.com>
2026-03-22 11:35:07 +0900

    [Platinum IV] Title: 숫자의 신, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Platinum/1422. 숫자의 신/숫자의 신.cc
+++ 백준/Platinum/1422. 숫자의 신/숫자의 신.cc
@@ -7,7 +7,7 @@ bool compareInit(const string &a, const string &b) {
     return a + b > b + a;
 }
 
-bool comparseBig(const string &a, const string &b) {
+bool biggerNumber(const string &a, const string &b) {
     if (a.length() == b.length()) {
         return a > b;
     }
@@ -15,55 +15,31 @@ bool comparseBig(const string &a, const string &b) {
     return a.length() > b.length();
 }
 
-string calculateString(const string &a, const string &b) {
-    if (a + b > b + a) {
-        return a + b;
-    }
-
-    return b + a;
-}
-
 int main() {
     int n, k;
     cin >> n >> k;
 
     vector<string> numbers;
+    string best = "";
     for (int i = 0; i < n; i++) {
         string num;
         cin >> num;
 
         numbers.push_back(num);
+        if (best.empty() || biggerNumber(num, best)) {
+            best = num;
+        }
     }
-
-    sort(numbers.begin(), numbers.end(), comparseBig);
-    string big_number = numbers[0];
     
-    vector<string> temp; // 작은 값 저장 공간
-    // 최종 순서
+    for (int i = 0; i < k - n; i++) {
+        numbers.push_back(best);
+    }
     sort(numbers.begin(), numbers.end(), compareInit);
 
-    // 이어붙일 수보다 작은 값 저장
-    while (numbers.back() < big_number) {
-        temp.push_back(numbers.back());
-        numbers.pop_back();
-    }
-    
     string answer = "";
     for (string s : numbers) {
         answer += s;
     }
-
-    int cnt = k - temp.size() - numbers.size();
-    while (cnt--) {
-        answer = calculateString(answer, big_number);    
-    }
-
-    string tail = "";
-    while (!temp.empty()) {
-        tail = calculateString(tail, temp.back());
-        temp.pop_back();
-    }
-
-    cout << answer + tail;
+    cout << answer;
     return 0;
 }
