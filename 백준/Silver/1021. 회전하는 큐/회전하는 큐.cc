b52849315cabed61fa02579a2ac50f4bbd8e4528
Wonel <jwjjwj1960@naver.com>
2026-03-28 23:38:32 +0900

    [Silver III] Title: 회전하는 큐, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    deque<int> dq;
+
+    for (int i = 1; i <= n; i++) {
+        dq.push_back(i);
+    }
+
+    int answer = 0;
+    for (int i = 0; i < m; i++) {
+        int a;
+        cin >> a;
+        auto it = find(dq.begin(), dq.end(), a);
+        int dist_front = distance(dq.begin(), it);
+        int dist_back = dq.size() - dist_front;
+
+        if (dist_front <= dist_back) {
+            rotate(dq.begin(), it, dq.end());
+            dq.pop_front();
+        } else {
+            rotate(dq.begin(), it, dq.end());
+            dq.pop_front();
+        }
+
+        answer += min(dist_front, dist_back);
+    }
+
+    cout << answer;
+    return 0;
+}
