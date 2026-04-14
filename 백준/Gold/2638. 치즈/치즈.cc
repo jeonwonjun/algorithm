ed78f4856b7364c7bf3cd856711442b6df70e5a7
Wonel <jwjjwj1960@naver.com>
2026-04-14 09:55:27 +0900

    [Gold III] Title: 치즈, Time: 12 ms, Memory: 2076 KB -BaekjoonHub

--- 백준/Gold/2638. 치즈/치즈.cc
+++ 백준/Gold/2638. 치즈/치즈.cc
@@ -1,29 +1,44 @@
-#include<iostream>
-#include<queue>
-#include<vector>
-#include<cstring>
+#include <iostream>
+#include <queue>
+#include <cstring>
 using namespace std;
+
 int n, m;
-int dx[4]={0,0,1,-1};
-int dy[4]={1,-1,0,0};
 int board[102][102];
 bool visited[102][102];
 
-void findOutSide(int sx, int sy) {
+int dx[4] = {0, 0, 1, -1};
+int dy[4] = {1, -1, 0, 0};
+
+void resetAir() {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (board[i][j] == -1) {
+                board[i][j] = 0;
+            }
+        }
+    }
+}
+
+void findOutside() {
+    memset(visited, false, sizeof(visited));
+
     queue<pair<int, int>> q;
-    q.push({sx, sy});
-    visited[sx][sy] = true;
+    q.push({0, 0});
+    visited[0][0] = true;
+    board[0][0] = -1;
+
     while (!q.empty()) {
-        int x = q.front().first;
-        int y = q.front().second;
+        auto [x, y] = q.front();
         q.pop();
+
         for (int dir = 0; dir < 4; dir++) {
             int nx = x + dx[dir];
             int ny = y + dy[dir];
-            if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
-                continue;
-            }
-            if (!visited[nx][ny] && (board[nx][ny] == 0 || board[nx][ny] == -1)) {
+
+            if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+
+            if (!visited[nx][ny] && board[nx][ny] == 0) {
                 visited[nx][ny] = true;
                 board[nx][ny] = -1;
                 q.push({nx, ny});
@@ -32,53 +47,55 @@ void findOutSide(int sx, int sy) {
     }
 }
 
+bool meltCheese() {
+    bool melted = false;
+    int temp[102][102];
+    memcpy(temp, board, sizeof(board));
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (board[i][j] == 1) {
+                int cnt = 0;
+
+                for (int dir = 0; dir < 4; dir++) {
+                    int nx = i + dx[dir];
+                    int ny = j + dy[dir];
+
+                    if (board[nx][ny] == -1) {
+                        cnt++;
+                    }
+                }
+
+                if (cnt >= 2) {
+                    temp[i][j] = 0;
+                    melted = true;
+                }
+            }
+        }
+    }
+
+    memcpy(board, temp, sizeof(board));
+    return melted;
+}
+
 int main() {
     cin >> n >> m;
-    queue<pair<int, int>> cheese;
+
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             cin >> board[i][j];
-            if (board[i][j] == 1) {
-                cheese.push({i, j});
-            }
         }
     }
-    findOutSide(0, 0);
-    int index = cheese.size();
-    vector<pair<int, int>> v;
+
     int hours = 0;
+
     while (true) {
-        index = cheese.size();
-        if (cheese.empty()) {
-            break;
-        }
-        while (index--) {
-            int x = cheese.front().first;
-            int y = cheese.front().second;
-            cheese.pop();
-            int count = 0;
-            for (int dir = 0; dir < 4; dir++) {
-                int nx = x + dx[dir];
-                int ny = y + dy[dir];
-                if (board[nx][ny] == -1) {
-                    count++;
-                }
-            }
-            if (count >= 2) {
-                v.push_back({x, y});
-            } else {
-                cheese.push({x, y});
-            }
-        }
-        while (!v.empty()) {
-            pair<int, int> last = v.back();
-            board[last.first][last.second] = 0;
-            v.pop_back();
-        }
-        memset(visited, false, sizeof(visited));
-        findOutSide(0, 0);
+        resetAir();
+        findOutside();
+        if (!meltCheese()) break;
         hours++;
     }
+
     cout << hours;
     return 0;
 }
