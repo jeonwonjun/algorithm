ce112ae89bf2811862789c99bc5ec7e7ec4d7efa
Wonel <jwjjwj1960@naver.com>
2026-08-02 15:23:16 +0900

    Time: 2 ms (96.34%), Space: 45.7 MB (51.87%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,7 +1,6 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> answer = new ArrayList<>();
-        Arrays.sort(candidates);
         backtracking(answer, new ArrayList<>(), candidates, target, 0);
         return answer;
     }
