e56112d2657a69ed218b7187263d5505db0db41b
Wonel <jwjjwj1960@naver.com>
2026-08-08 15:06:09 +0900

    [level 3] Title: 표 편집, Time: 85.15 ms, Memory: 155 MB -BaekjoonHub

---
+++ 프로그래머스/3/81303. 표 편집/표 편집.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int n, int k, String[] cmd) {
+        Stack<Integer> deleted = new Stack<>();
+        int[] up = new int[n+2];
+        int[] down = new int[n+2];
+        
+        for (int i = 0; i < n+2; i++) {
+            up[i] = i-1;
+            down[i] = i+1;
+        }
+        
+        k++;
+        
+        for (String s : cmd) {
+            if (s.startsWith("C")) {
+                deleted.push(k);
+                up[down[k]] = up[k];
+                down[up[k]] = down[k];
+                k = n < down[k] ? up[k] : down[k];
+            } else if (s.startsWith("Z")) {
+                int restore = deleted.pop();
+                down[up[restore]] = restore;
+                up[down[restore]] = restore;
+            } else {
+                String[] sx = s.split(" ");
+                int x = Integer.parseInt(sx[1]);
+                for (int i = 0; i < x; i++) {
+                    k = sx[0].equals("U") ? up[k] : down[k];
+                }
+            }
+        }
+        
+        char[] answer = new char[n];
+        Arrays.fill(answer, 'O');
+        
+        for (int i : deleted) {
+            answer[i - 1] = 'X';
+        }
+        
+        return new String(answer);
+    }
+}
